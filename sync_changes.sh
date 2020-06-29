1ce9c3ea212bfc20dff4f041baf0a9926be09e7e
Daniel Reuter <daniel.reuter@atea.se>
2020-06-29 04:09:41 +0200

    change

--- sync_changes.sh
+++ sync_changes.sh
@@ -11,10 +11,12 @@ rsync -av $USERHOME/.xinitrc ~/.xinitrc
 rsync -av $USERHOME/.Xmodmap ~/.Xmodmap
 rsync -av $USERHOME/.xmobarrc ~/.xmobarrc
 sudo rsync -v $SYSTEM/etc/profile.conf /etc/profile.conf
+sudo rsync -v $SYSTEM/etc/gdm/custom.conf /etc/gdm/custom.conf
 sudo rsync -v $USERHOME/.config/i3/i3status.conf /etc/i3status.conf
 sudo mkdir -p /usr/share/backgrounds/arch; sudo rsync -rv $SYSTEM/usr/share/backgrounds/arch/* /usr/share/backgrounds/arch
-
-
+sudo mkdir -p /etc/default; sudo rsync -v $SYSTEM/etc/default/grub /etc/default/grub
+sudo mkdir -p /etc/X11/xorg.conf.d; sudo rsync -v $SYSTEM/etc/X11/xorg.conf.d/* /etc/X11/xorg.conf.d/
+sudo mkdir -p /etc/X11; sudo rsync -v $SYSTEM/etc/X11/xorg.conf /etc/X11/xorg.conf
 
 # home dirs
 #mkdir -p ~/.screenlayout; rsync -av $USERHOME/.screenlayout/* ~/.screenlayout/
@@ -23,8 +25,7 @@ sudo mkdir -p /usr/share/backgrounds/arch; sudo rsync -rv $SYSTEM/usr/share/back
 #/etc root
 #/etc sub
 #sudo mkdir -p /etc/X11; sudo rsync -v $SYSTEM/etc/X11/xorg.conf /etc/X11/xorg.conf
-#sudo mkdir -p /etc/X11/xorg.conf.d; sudo rsync -v $SYSTEM/etc/X11/xorg.conf.d/10-monitor.conf /etc/X11/xorg.conf.d/10-monitor.conf
+#
 #sudo mkdir -p /etc/profile.d; sudo rsync -v $SYSTEM/etc/profile.d/hidpi.sh /etc/profile.d/hidpi.sh
 #sudo mkdir -p /etc/lightdm; sudo rsync -v $SYSTEM/etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
-#sudo mkdir -p /etc/default; sudo rsync -v $SYSTEM/etc/default/grub /etc/default/grub
 #/usr sub
